57e0868c3fee577c33e08ecf738cc46b8d9e7a4b
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2024-12-06 19:57:24 +0200

    Added assembly info files

--- school-diary-system/obj/Debug/net8.0/school-diary-system.AssemblyInfo.cs
+++ school-diary-system/obj/Debug/net8.0/school-diary-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("school-diary-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5951d97f8dd4a451f38e56b18fdde6d3ea93bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2326ad82808f8b2b41562a71d7d3409a1f7e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("school-diary-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("school-diary-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
